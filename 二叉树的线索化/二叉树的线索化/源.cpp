261373d4b61ac8c1424c2201e158944827e791ab
AnyAdapter <2641047194@qq.com>
2017-04-10 23:47:55 +0800

    four

--- 二叉树的线索化/二叉树的线索化/源.cpp
+++ 二叉树的线索化/二叉树的线索化/源.cpp
@@ -1,2 +1,27 @@
 #include<stdio.h>
 #include<stdlib.h>
+
+//声明数据类型
+typedef int DataType;
+
+//线索化的二叉树数据类型声明
+typedef struct node {
+	int ltag, rtag;
+	DataType data;
+	struct node *lchild, *rchild;
+}Threadnode,*ThreadTree;
+
+//声明函数
+void InThreah();//中序线索化
+
+//主函数入口
+int main()
+{
+	return 0;
+
+}
+//中序线索化
+void InThread(ThreadTree t, ThreadTree pre)
+{
+
+}
